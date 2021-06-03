b753d180c8818400dab45baf123c98cc03d28824
Dima <ifffweknow@gmail.com>
2021-06-03 15:23:50 +0300

    add example

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import HTMLView from './view/HTMLView';
-import Game from './model/Game';
-import TicTacToe from './games/TickTacToe';
-
-const game = new Game({
-  playersList: ['player One', 'player Two'],
-  strategy: new TicTacToe(),
-  fieldSize: { x: 3, y: 3 },
-});
-
-const HTMLViewGame = new HTMLView(game);
-HTMLViewGame.render('.main');
+export * from './model/Game';
+export * from './view/HTMLView';
+export * from './games/Strategy';
