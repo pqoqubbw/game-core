bee5cd2fb5814fce5d58462dee104b661086e107
Dima <ifffweknow@gmail.com>
2021-06-07 12:43:49 +0300

    add export Strategy

--- example/demo/index.js
+++ example/demo/index.js
@@ -1,3 +1,4 @@
 export { default as Game } from './model/Game.js';
 export { default as HTMLView } from './view/HTMLView.js';
 export { default as TicTacToe } from './games/TickTacToe.js';
+export { default as Strategy } from './games/Strategy.js';
